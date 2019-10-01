d08e48d0f81b0a96e39fe94041363a99182e7332
Jake Miller <jake0miller@gmail.com>
2019-10-01 16:41:31 -0600

    Write user creation spec

--- test/user.spec.js
+++ test/user.spec.js
@@ -1,19 +1,43 @@
 var shell = require('shelljs');
 var request = require("supertest");
-var index = require('../routes/index');
+var app = require('../app');
 
-describe('Test the root path', () => {
-  test('It should respond to the GET method', () => {
-    return request(index).get("/").then(response => {
-      expect(response.statusCode).toBe(200)
-    })
-  });
-});
+describe('api', () => {
 
-describe('Test user creation', () => {
-  test('It should respond to the POST method', () => {
-    return request(index).get("/api/v1/users").then(response => {
-      expect(response.statusCode).toBe(200)
-    })
+  beforeAll(() => {
+    shell.exec('npx sequelize db:create')
   });
+  beforeEach(() => {
+    shell.exec('npx sequelize db:migrate')
+    shell.exec('npx sequelize db:seed:all')
+  });
+  afterEach(() => {
+    shell.exec('npx sequelize db:migrate:undo:all')
+  });
+
+  describe('Test POST /api/v1/users path', () => {
+    test('should return a 201 status', () => {
+      return request(app).get("/api/v1/users").then(response => {
+        expect(response.status).toBe(201)
+      })
+    });
+
+    var service = {
+      title: "Mario",
+      price: 500,
+      releaseYear: 1985,
+      active: true
+    };
+
+    test('should return an api key', () => {
+      return request(app)
+        .post("/api/v1/users")
+        .send(service)
+        .then(response => {
+          expect(Object.keys(response.body)).toContain('api_key')
+          expect(Object.keys(response.body).length).toBe(1)
+      })
+    });
+  });
+
 });

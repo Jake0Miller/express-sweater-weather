59b70fb0b4e727e22a584218f7761829a039174f
Jake Miller <jake0miller@gmail.com>
2019-10-01 07:20:24 -0600

    Add bcrypt to user creation and format json response to return generic api key

--- routes/api/v1/users.js
+++ routes/api/v1/users.js
@@ -1,21 +1,25 @@
 var express = require('express');
 var router = express.Router();
+const bcrypt = require('bcrypt');
 var User = require('../../../models').User;
 
 /* Post new user */
 router.post('/', function(req, res, next) {
-  User.create({
-          email: req.body.email,
-          password: req.body.password
+  bcrypt.hash(req.body.password, 10, function(err, hash) {
+    User.create({
+      email: req.body.email,
+      password: hash,
+      api_key: 0
     })
     .then(user => {
       res.setHeader("Content-Type", "application/json");
-      res.status(201).send(JSON.stringify(user));
+      res.status(201).send(JSON.stringify({"api_key": user.api_key}));
     })
     .catch(error => {
       res.setHeader("Content-Type", "application/json");
       res.status(500).send({ error });
     });
+  });
 });
 
 module.exports = router;

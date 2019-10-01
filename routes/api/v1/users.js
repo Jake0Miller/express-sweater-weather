1b1a6eb425ee30f465430351e09a722eef2b4c3f
Jake Miller <jake0miller@gmail.com>
2019-10-01 16:33:39 -0600

    Add secure random string for api key generation

--- routes/api/v1/users.js
+++ routes/api/v1/users.js
@@ -7,17 +7,20 @@ var srs = require('secure-random-string');
 /* Post new user */
 router.post('/', function(req, res, next) {
   bcrypt.hash(req.body.password, 10, function(err, hash) {
+    res.setHeader("Content-Type", "application/json");
+
     User.create({
       email: req.body.email,
       password: hash,
       api_key: srs()
     })
+
     .then(user => {
-      res.setHeader("Content-Type", "application/json");
-      res.status(201).send(JSON.stringify({"api_key": user.api_key}));
+      payload = {"api_key": user.api_key};
+      res.status(201).send(JSON.stringify(payload));
     })
+
     .catch(error => {
-      res.setHeader("Content-Type", "application/json");
       res.status(500).send({ error });
     });
   });

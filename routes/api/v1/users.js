e27f81d1225e344befc45b08f694245829111147
Jake Miller <jake0miller@gmail.com>
2019-10-01 19:22:47 -0600

    Fix .sequelizerc to route to correct location for database files

--- routes/api/v1/users.js
+++ routes/api/v1/users.js
@@ -39,24 +39,24 @@ router.post('/', function(req, res, next) {
       res.status(400).send(JSON.stringify(payload));
     }
 
-    // User.findOne({
-    //   where: {email: req.body.email}
-    // })
-    //
-    // .then(users => {
-    //   if (users.length > 0) {
-    //     payload = {
-    //       error: 'EmailAlreadyTaken',
-    //       status: 400,
-    //       message: 'Email has already been taken.'
-    //     }
-    //     res.status(400).send(JSON.stringify(payload));
-    //   }
-    // })
-    //
-    // .catch(error => {
-    //   res.status(500).send({ error });
-    // });
+    User.findOne({
+      where: { email: req.body.email }
+    })
+
+    .then(user => {
+      if (user) {
+        payload = {
+          error: 'EmailAlreadyTaken',
+          status: 400,
+          message: 'Email has already been taken.'
+        }
+        res.status(400).send(JSON.stringify(payload));
+      }
+    })
+
+    .catch(error => {
+      res.status(500).send({ error });
+    });
 
     User.create({
       email: req.body.email,
